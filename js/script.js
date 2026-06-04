6269091af5d849aaeb2e8fed0172ad99dc38bf4b
Rgithubpro <ralph@broekaart.nl>
2026-06-04 10:25:45 +0200

    make microsfot button work

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 const loading_screen = document.getElementById('loading-screen');
 loading_screen.style.display = "none";
+
+const microsoft = document.getElementById('welcome-screen-login-option-microsoft');
+microsoft.addEventListener('click', () => {
+    alert('Microsoft login is currently unavailable. Please try again later.');
+});
