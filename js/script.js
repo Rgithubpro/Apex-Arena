f0d6735ce34fafde824d32060c3f32cca0367236
Rgithubpro <ralph@broekaart.nl>
2026-06-04 14:43:55 +0200

    hide welcom

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
+// Hide parts i dont want to see currently
 const loading_screen = document.getElementById('loading-screen');
 loading_screen.style.display = "none";
+const welcome_screen = document.getElementById('welcome-screen');
+welcome_screen.style.display = "none";
+
+
+
+
+
+
 
 const microsoft = document.getElementById('welcome-screen-login-option-microsoft');
 microsoft.addEventListener('click', () => {
