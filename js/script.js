d3071b9e205b713f1a0aba328fa1404d3436d74f
Rgithubpro <ralph@broekaart.nl>
2026-06-05 08:11:46 +0200

    add router.js

--- js/script.js
+++ js/script.js
@@ -2,15 +2,25 @@
 const loading_screen = document.getElementById('loading-screen');
 loading_screen.style.display = "none";
 const welcome_screen = document.getElementById('welcome-screen');
-welcome_screen.style.display = "none";
+welcome_screen.style.display = "visible";
 
 
 
 
 
 
-
+// Random pieces js for now.
 const microsoft = document.getElementById('welcome-screen-login-option-microsoft');
 microsoft.addEventListener('click', () => {
     alert('Microsoft login is currently unavailable. Please try again later.');
 });
+const show_password = document.getElementById('welcome-screen-email-login-show-password');
+const show_password_icon = document.getElementById('welcome-screen-email-login-show-password-icon');
+let password_visible = false;
+show_password.addEventListener('click', () => {
+    password_visible = !password_visible;
+    if (password_visible) {
+        show_password_icon.src = 'assets/images/icons/hide-password.png';
+    } else {
+        show_password_icon.src = 'assets/images/icons/show-password.png';
+    }});
